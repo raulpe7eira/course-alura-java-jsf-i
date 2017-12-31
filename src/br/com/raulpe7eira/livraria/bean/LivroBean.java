66469e4f7415304818ff3134df654ce7fd249a7f
Raul Pereira <raul.pereira@gmail.com>
2017-12-31 05:25:41 -0200

    Aula 02 - Configuração do ambiente e os primeiros passos com JSF

---
+++ src/br/com/raulpe7eira/livraria/bean/LivroBean.java
@@ -0,0 +1,18 @@
+package br.com.raulpe7eira.livraria.bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class LivroBean {
+
+    private Livro livro = new Livro();
+
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public void gravar() {
+		System.out.println("Gravando livro " + this.livro.getTitulo());
+    }
+
+}
